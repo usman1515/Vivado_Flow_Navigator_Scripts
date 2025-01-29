c7e3e2556a8701aa16ed6010553ef6e56358b5d9
usman1515 <usmansiddique1515@gmail.com>
2025-01-29 15:56:05 +0100

    better verbosity.
    override existing bitstream if it already exists.
    added timing, methodology, drc, power reports.

--- gen_bitstream.tcl
+++ gen_bitstream.tcl
@@ -6,21 +6,28 @@ source ./scripts/color_func.tcl
 set start_time [clock seconds]
 
 # * open previous checkpoint
-print_green "reading checkpoint: ${name_chkp_impl5}"
+print_yellow "reading checkpoint: ${name_chkp_impl5}"
 open_checkpoint ${dir_chkp}/${name_chkp_impl5}.dcp
 
 # compress bitstream generation
 set_property BITSTREAM.GENERAL.COMPRESS TRUE [current_design]
 
 # * generate bitstream
-print_green "generating bitstream"
-write_bitstream -force ${prj_dir}/${name_bitstream}.bit
+print_yellow "generating bitstream"
+write_bitstream -force -verbose ${prj_dir}/${name_bitstream}.bit
 
 # * write reports
-print_green "writing reports: generate bitstream"
-report_clocks -file ${dir_rpt}/${name_rpt_clk6}.rpt
-report_timing_summary -file ${dir_rpt}/${name_rpt_timing6}.rpt
-report_utilization -file ${dir_rpt}/${name_rpt_util6}.rpt
+print_yellow "writing reports: generate bitstream"
+print_blue "writing report_timing_summary"
+report_timing_summary -check_timing_verbose -delay_type min_max -max_paths 10 -report_unconstrained -input_pins -routable_nets -file ${dir_rpt}/${name_rpt_bit}_timing_summary.rpt
+print_blue "writing report_methodology"
+report_methodology -file ${dir_rpt}/${name_rpt_bit}_methodology.rpt
+print_blue "writing report_drc"
+report_drc -ruledecks {default opt_checks placer_checks router_checks bitstream_checks incr_eco_checks eco_checks abs_checks} -file ${dir_rpt}/${name_rpt_bit}_drc.rpt
+print_blue "writing report_power"
+report_power -file ${dir_rpt}/${name_rpt_bit}_power.rpt
+
+
 
 # * get elapsed time
 # get end time
@@ -37,4 +44,4 @@ set seconds [expr {$rem_sec % 60}]
 # print total time taken
 print_blue "Simulation started at:  [clock format $start_time -format "%d-%b-%Y - %I:%M:%S - %p"]"
 print_blue "Simulation ended at:    [clock format $end_time -format "%d-%b-%Y - %I:%M:%S - %p"]"
-print_red "Generate Bitstream time taken: [format \"%02d:%02d:%02d:%02d\" $days $hours $minutes $seconds]"
+print_green "Generate Bitstream time taken: [format \"%02d:%02d:%02d:%02d\" $days $hours $minutes $seconds]"
