5eb8876efc42e4618863de07eb625aa33572d4f2
usman1515 <usmansiddique1515@gmail.com>
2025-01-31 01:15:59 +0100

    better verbosity. removed all report generation.

--- impl_s4_phys_opt_design.tcl
+++ impl_s4_phys_opt_design.tcl
@@ -2,34 +2,28 @@
 
 source ./scripts/color_func.tcl
 
-# * get start time
+# get start time
 set start_time [clock seconds]
 
-# * open previous checkpoint
-print_green "reading checkpoint: ${name_chkp_impl3}"
+
+# open previous checkpoint
+print_yellow "reading checkpoint: ${name_chkp_impl3}"
 open_checkpoint ${dir_chkp}/${name_chkp_impl3}.dcp
 
-# * run phys_opt_design
-print_green "running implementation phase: phys_opt_design"
-phys_opt_design \
-    -fanout_opt \
-    -placement_opt \
-    -rewire \
-    -dsp_register_opt \
-    -shift_register_opt \
-    -retime
-# -routing_opt
-# -directive Default
+# run phys_opt_design
+print_yellow "running implementation phase: phys_opt_design"
 
-# * write checkpoint
-print_green "writing checkpoint: ${name_chkp_impl4}"
+phys_opt_design \
+    -bram_register_opt -dsp_register_opt -shift_register_opt \
+    -fanout_opt -placement_opt \
+    -rewire -retime \
+    -verbose
+
+# write checkpoint
+print_yellow "writing checkpoint: ${name_chkp_impl4}"
 write_checkpoint -force ${dir_chkp}/${name_chkp_impl4}.dcp
 
-# * write reports
-print_green "writing reports: post implementation phys_opt_design"
-report_clocks -file ${dir_rpt}/${name_rpt_clk4}.rpt
-report_timing_summary -file ${dir_rpt}/${name_rpt_timing4}.rpt
-report_utilization -file ${dir_rpt}/${name_rpt_util4}.rpt
+
 
 # * get elapsed time
 # get end time
@@ -46,4 +40,4 @@ set seconds [expr {$rem_sec % 60}]
 # print total time taken
 print_blue "Simulation started at:  [clock format $start_time -format "%d-%b-%Y - %I:%M:%S - %p"]"
 print_blue "Simulation ended at:    [clock format $end_time -format "%d-%b-%Y - %I:%M:%S - %p"]"
-print_red "Implementation (phys_opt_design) time taken: [format \"%02d:%02d:%02d:%02d\" $days $hours $minutes $seconds]"
+print_green "Implementation (phys_opt_design) time taken: [format \"%02d:%02d:%02d:%02d\" $days $hours $minutes $seconds]"
